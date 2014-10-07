996f3376a989a8ad9a0db33f6b5e214d1d351f0e
pkuzma101 <kuzma.paul@gmail.com>
2014-10-07 11:45:44 -0500

    Exercise 3.3.4 - #1

---
+++ break_continue.php
@@ -0,0 +1,18 @@
+<?php
+// for($i =1; $i <= 100; $i++) {
+// // 	echo $i . "\n";
+
+// // 	if($i == 5) {
+// // 		break;
+// // 	}
+// }
+for ($i = 1; $i <= 100; $i++) {
+    echo $i . "\n";
+    if ($i % 2 == 0) {
+        continue;
+    }
+    echo "^ that is an odd number.\n";
+}
+
+
+?>

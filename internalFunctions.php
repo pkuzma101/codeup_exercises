b2942f3831d15d2b6f592d49c5e5591ed706f8fb
pkuzma101 <kuzma.paul@gmail.com>
2014-10-08 13:01:03 -0500

    Exercise 3.5.3

---
+++ internalFunctions.php
@@ -0,0 +1,26 @@
+<?php
+
+$nothing = null;
+$something = " ";
+$array = [1, 2, 3];
+unset($nothing);
+
+if(isset($snothing)) {
+	echo "\$nothing is SET" . PHP_EOL;
+}
+else {
+	echo "\$nothing is EMPTY" . PHP_EOL;
+}
+
+if(isset($something)) {
+	echo "\$something is SET" . PHP_EOL;
+}
+
+$array = serialize($array);
+	echo $array . PHP_EOL;
+
+$array = unserialize($array);
+	echo $array . PHP_EOL;
+
+
+?>

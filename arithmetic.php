6460ac60f511501def70d34ab8e90f9203830e77
pkuzma101 <kuzma.paul@gmail.com>
2014-10-08 09:27:37 -0500

    Exerise 3.5 - #2

--- arithmetic.php
+++ arithmetic.php
@@ -1,20 +1,27 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b) {
-    echo $a - $b;
+    echo $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b) {
-    echo $a * $b;
+    echo $a * $b . PHP_EOL;
 }
 
 function divide($a, $b) {
-    echo $a / $b;
+    echo $a / $b . PHP_EOL;
 }
 
+add(6, 8);
+
+subtract(27, 10);
+
+multiply(8, 53);
+
+divide(64, 8);
 
 ?>
